d305677ac01ca5c4bdb6c7dbb79c688109a549c2
gary-burke <garylewisburke@gmail.com>
2026-02-28 15:01:38 +0100

    Set up project for heroku deployment

--- bake_my_cake/settings.py
+++ bake_my_cake/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1mdcpc%ji#n9nd+n-%-z*@cdw$@d7f$47toj$bai3&%!o#q8&v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '.127.0.0.1', ]
 
 
 # Application definition
