95d751085b55b8bbff8404a096c782478fff6d97
gary-burke <garylewisburke@gmail.com>
2026-03-03 20:20:41 +0100

    Change name for cakes template to products and update views and urls accordingly

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cakes/', views.CakeList.as_view(), name='cakes'),
+    path('', views.ProductList.as_view(), name='products'),
 ]
