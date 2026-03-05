6f6ffc3288b6a4f7800834c3b5afec688d7b7f60
gary-burke <garylewisburke@gmail.com>
2026-03-05 21:27:42 +0100

    Add scroll to top button

--- products/static/products/js/products.js
+++ products/static/products/js/products.js
@@ -46,4 +46,36 @@ $(document).ready(function () {
         window.location.replace(currentUrl);
     });
 
+    
+    let mybutton = document.getElementById("scroll-top");
+
+    $(mybutton).hover(function () {
+        $(this).toggleClass("fa-beat");
+    });
+
+    
+    // https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+    window.onscroll = function () {
+        scrollFunction()
+    };
+    
+    function scrollFunction() {
+        if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+
+    mybutton.addEventListener("click", () => {
+        topFunction();
+    })
+
+    
+
 });
