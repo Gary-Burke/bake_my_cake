df0931ce91392072958801b9002cb5aa1c37cbad
gary-burke <garylewisburke@gmail.com>
2026-03-08 14:44:04 +0100

    Add trailing slash to url path

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductList.as_view(), name='products'),
-    path('<slug:slug>/<int:product_id>',
+    path('<slug:slug>/<int:product_id>/',
          views.product_details, name='product_details'),
 ]
