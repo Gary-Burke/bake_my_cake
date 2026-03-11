d75e891a86aee69fdc65faccee93ac7bbf110c8b
gary-burke <garylewisburke@gmail.com>
2026-03-11 21:44:47 +0100

    Add template and url for add product

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.ProductList.as_view(), name='products'),
     path('<slug:slug>/<int:product_id>/',
          views.product_details, name='product_details'),
+    path('add/', views.add_product, name='add_product'),
 ]
