e307e1a470f337d3d5dbd6ff08354d16a220955e
gary-burke <garylewisburke@gmail.com>
2026-03-11 21:41:57 +0100

    Rearrange fields in admin list display

--- products/admin.py
+++ products/admin.py
@@ -5,7 +5,7 @@ from .models import Category, Product
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
 
-    list_display = ("category", "name", "base_price", "updated_on")
+    list_display = ("name", "category", "base_price", "updated_on")
     search_fields = ("name", "shape")
     list_filter = ("category", "shape")
     prepopulated_fields = {'slug': ('name',)}
