16e3dd9cb76e2a6d047db788e2c93056e6c4d085
gary-burke <garylewisburke@gmail.com>
2026-03-15 17:18:47 +0100

    Add functionality to send admin an email when a custom order is placed

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .forms import CustomOrderForm
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+
 
 # Create your views here.
 
@@ -16,15 +20,33 @@ def about(request):
 def custom_order(request):
     custom_order_form = CustomOrderForm()
 
+    def _send_confirmation_email(custom_order):
+        """Send the user a confirmation email"""
+        subject = render_to_string(
+            'pages/custom_order_emails/custom_order_subject.txt',
+            {'custom_order': custom_order})
+        body = render_to_string(
+            'pages/custom_order_emails/custom_order_body.txt',
+            {'custom_order': custom_order,
+             'contact_email': settings.DEFAULT_FROM_EMAIL})
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [settings.DEFAULT_FROM_EMAIL]
+        )
+
     if request.method == "POST":
         custom_order_form = CustomOrderForm(
             data=request.POST, files=request.FILES)
         if custom_order_form.is_valid():
-            custom_order_form.save()
+            custom_order = custom_order_form.save()
             messages.add_message(
                 request, messages.SUCCESS,
                 "Custom order submitted successfully!"
             )
+            _send_confirmation_email(custom_order)
             custom_order_form = CustomOrderForm()
 
     template = "pages/custom_order.html"
