a9b3fee3114c579f80cb6c09fad38ff890b237fc
gary-burke <garylewisburke@gmail.com>
2026-03-17 08:49:33 +0100

    Add tags field and placeholders to Product Form

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from cloudinary.forms import CloudinaryFileField
-from .models import Product
+from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
@@ -8,8 +8,20 @@ class ProductForm(forms.ModelForm):
     Form to add products to database for superuser
     """
     image_url = CloudinaryFileField(
-        options={'folder': 'products'},
-        required=False
+        label="Image Upload", options={'folder': 'products'},
+        required=True)
+    name = forms.CharField(
+        max_length=255, label="Name of Cake",
+        widget=forms.TextInput(
+            attrs={"placeholder": "e.g. Death by Chocolate"})
+    )
+    tags = forms.CharField(
+        max_length=255, label="Search Tags",
+        widget=forms.TextInput(
+            attrs={"placeholder": "e.g. Frozen, Disney, Elsa"}))
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        required=True
     )
 
     class Meta:
@@ -18,11 +30,8 @@ class ProductForm(forms.ModelForm):
         """
         model = Product
         fields = (
-            "name",
-            "shape",
-            "category",
-            "base_price",
-            "image_url",
+            "name", "tags", "shape", "category",
+            "base_price", "image_url",
         )
 
 
@@ -41,10 +50,6 @@ class EditProductForm(forms.ModelForm):
         """
         model = Product
         fields = (
-            "name",
-            "tags",
-            "shape",
-            "category",
-            "base_price",
-            "image_url",
+            "name", "tags", "shape", "category",
+            "base_price", "image_url",
         )
