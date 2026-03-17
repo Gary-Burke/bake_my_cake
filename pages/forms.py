e0874942a1ec3c975c967c03259d455d766ee4b0
gary-burke <garylewisburke@gmail.com>
2026-03-17 08:34:07 +0100

    Add placeholders to Custom Order Form and adjust cloudinary field

--- pages/forms.py
+++ pages/forms.py
@@ -8,9 +8,28 @@ class CustomOrderForm(forms.ModelForm):
     Form for user to submit custom order.
     """
     image_url = CloudinaryFileField(
-        options={'folder': 'products'},
-        required=False
-    )
+        label="Image Upload", options={'folder': 'custom orders'},
+        required=False)
+    name = forms.CharField(max_length=255, widget=forms.TextInput(
+        attrs={"placeholder": "e.g. John"}))
+    email = forms.EmailField(max_length=255, widget=forms.TextInput(
+        attrs={"placeholder": "e.g. John@testmail.com"}))
+    shape = forms.CharField(max_length=255, widget=forms.TextInput(
+        attrs={"placeholder": "e.g. Hexagon, L-Shaped or Heart"}))
+    size = forms.CharField(max_length=255, widget=forms.TextInput(
+        attrs={"placeholder": "e.g. 20 x 30cm"}))
+    tiers = forms.IntegerField(widget=forms.TextInput(
+        attrs={"placeholder": "e.g. 1 (we offer 1-3 tier options)"}))
+    sponge = forms.CharField(max_length=255, widget=forms.TextInput(
+        attrs={"placeholder": "e.g. Red Velvet"}))
+    filling = forms.CharField(max_length=255, widget=forms.TextInput(
+        attrs={"placeholder": "e.g. Caramel Treat"}))
+    icing = forms.CharField(max_length=255, widget=forms.TextInput(
+        attrs={"placeholder": "e.g. Buttercream"}))
+    additional_info = forms.CharField(
+        max_length=255, required=False, widget=forms.TextInput(
+            attrs={"placeholder": "e.g. Paw Patrol, "
+                   "6 year old boy, birthday party"}))
 
     class Meta:
         """
