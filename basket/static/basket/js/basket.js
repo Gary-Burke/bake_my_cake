dfd73d08331ca22b38a8a681ab64fd401dc2b344
gary-burke <garylewisburke@gmail.com>
2026-03-21 12:35:45 +0100

    Add desktop and mobile templates to view the basket

---
+++ basket/static/basket/js/basket.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+    // Add animation to edit/delete icons when hovered
+    $(".basket-icons i").hover(function () {
+        $(this).toggleClass("fa-beat");
+    });
+
+});
