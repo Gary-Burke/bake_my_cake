3c22d7e2e67408e98c1483016470ed42139ed3db
gary-burke <garylewisburke@gmail.com>
2026-03-25 21:20:32 +0100

    Register models in admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import DeliveryDate, Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+
+    list_display = ("order_number", "name_surname",
+                    "grand_total", "created_on", "delivery_date",)
+    search_fields = ("order_number", "name_surname",)
+    list_filter = ("name_surname", "delivery_date",)
+
 
 # Register your models here.
+admin.site.register(DeliveryDate)
