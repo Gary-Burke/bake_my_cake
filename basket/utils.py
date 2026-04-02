29e0a636b5d2c28b8dc5c3d2cd42c69b316dbe1b
gary-burke <garylewisburke@gmail.com>
2026-04-02 13:18:43 +0200

    Refactor logic for add and edit items in basket to check for quantity to separate file utils.py

---
+++ basket/utils.py
@@ -0,0 +1,79 @@
+from decimal import Decimal
+
+
+def check_basket(request, basket, data, product, total):
+    """
+    This functions checks newly added item and edit items
+    to check if they match any of the items in the basket.
+    If all details match, then the qty and amount of the item in
+    the basket is increased. If not, then a new item is added.
+    """
+
+    data = request.POST
+
+    # Make a copy of POST data and prepare the data
+    # to compare with basket line items
+    qty_check = {"check": dict(data.copy())}
+    qty_check["check"].pop("csrfmiddlewaretoken")
+    qty_check["check"].pop("quantity")
+
+    # Some products don't have tiers as options, so set default to 1
+    qty_check["check"]["tiers"] = int(data.get("tiers", 1))
+
+    # from POST dictionary turn values from list to string
+    for key, value in qty_check["check"].items():
+        if isinstance(value, list):
+            qty_check["check"][key] = value[0]
+
+    qty_check["check"].update(
+        {"name": product.name, "product_id": product.id})
+
+    qty_change = False
+
+    # compare POST data with basket items to find duplicates and update
+    # quantity and total for items in basket accordingly.
+    # Remove quantity and total for dictionary comparison as these should
+    # be diffent values while the dictionaries still match.
+    for item in basket:
+        quantity_item = int(basket[item].pop("quantity"))
+        total_item = basket[item].pop("total")
+
+        print(f"qty_check: {qty_check["check"]}")
+        print(f"basket: {basket[item]}")
+
+        if basket[item] == qty_check["check"]:
+            basket[item]["quantity"] = quantity_item + \
+                int(data.get("quantity"))
+            basket[item]["total"] = str(
+                Decimal(total_item) + Decimal(total))
+            qty_change = True
+        else:
+            basket[item]["quantity"] = quantity_item
+            basket[item]["total"] = total_item
+
+    # If items don't match then add a new item to the basket
+    # Ensure item_id is unique or it will overwrite items in basket
+    if not qty_change:
+        current_items = [x for x in basket]
+        counter = 1
+        item_id = "item_" + str(len(basket) + counter)
+
+        while item_id in current_items:
+            counter += 1
+            item_id = "item_" + str(len(basket) + counter)
+
+        basket[item_id] = {}
+        basket[item_id]["name"] = product.name
+        basket[item_id]["product_id"] = product.id
+        basket[item_id]["size"] = data.get("size")
+        basket[item_id]["tiers"] = int(data.get("tiers", 1))
+        basket[item_id]["sponge"] = data.get("sponge")
+        basket[item_id]["filling"] = data.get("filling")
+        basket[item_id]["icing"] = data.get("icing")
+        basket[item_id]["main_colour"] = data.get("main_colour")
+        basket[item_id]["secondary_colour"] = data.get("secondary_colour")
+        basket[item_id]["quantity"] = int(data.get("quantity", 1))
+        basket[item_id]["cake_topper"] = data.get("cake_topper")
+        basket[item_id]["total"] = total
+
+    request.session["basket"] = basket
--- basket/views.py
+++ basket/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.contrib import messages
 from django.http import JsonResponse
+
 from products.models import Product
-from decimal import Decimal
+from .utils import check_basket
 from products.utils import calculate_total
 from products.constants import PRODUCT_COST, PRODUCT_COST_CUPCAKE
 
@@ -22,65 +23,11 @@ def add_to_basket(request, product_id):
     **Model**
     :model:`products.Product`
     """
-
     if request.method == "POST":
         product = get_object_or_404(Product, pk=product_id)
-        data = request.POST
         basket = request.session.get("basket", {})
         total = calculate_total(request, product)
-
-        # Make a copy of POST data and prepare the data
-        # to compare with basket line items
-        qty_check = {"check": dict(data.copy())}
-        qty_check["check"].pop("csrfmiddlewaretoken")
-        qty_check["check"].pop("quantity")
-
-        # Some products don't have tiers as options, so set default to 1
-        qty_check["check"]["tiers"] = int(data.get("tiers", 1))
-
-        # from POST dictionary turn values from list to string
-        for key, value in qty_check["check"].items():
-            if isinstance(value, list):
-                qty_check["check"][key] = value[0]
-
-        qty_check["check"].update(
-            {"name": product.name, "product_id": product_id})
-        qty_change = False
-
-        # compare POST data with basket items to find duplicates and update
-        # quantity and total for items in basket accordingly.
-        # Remove quantity and total for dictionary comparison as these should
-        # be diffent values while the dictionaries still match.
-        for item in basket:
-            quantity_item = int(basket[item].pop("quantity"))
-            total_item = basket[item].pop("total")
-            if basket[item] == qty_check["check"]:
-                basket[item]["quantity"] = quantity_item + \
-                    int(data.get("quantity"))
-                basket[item]["total"] = str(
-                    Decimal(total_item) + Decimal(total))
-                qty_change = True
-            else:
-                basket[item]["quantity"] = quantity_item
-                basket[item]["total"] = total_item
-
-        if not qty_change:
-            item_id = "item_" + str(len(basket) + 1)
-            basket[item_id] = {}
-            basket[item_id]["name"] = product.name
-            basket[item_id]["product_id"] = product_id
-            basket[item_id]["size"] = data.get("size")
-            basket[item_id]["tiers"] = int(data.get("tiers", 1))
-            basket[item_id]["sponge"] = data.get("sponge")
-            basket[item_id]["filling"] = data.get("filling")
-            basket[item_id]["icing"] = data.get("icing")
-            basket[item_id]["main_colour"] = data.get("main_colour")
-            basket[item_id]["secondary_colour"] = data.get("secondary_colour")
-            basket[item_id]["quantity"] = int(data.get("quantity", 1))
-            basket[item_id]["cake_topper"] = data.get("cake_topper")
-            basket[item_id]["total"] = total
-
-        request.session["basket"] = basket
+        check_basket(request, basket, product, total)
 
         messages.add_message(
             request, messages.SUCCESS,
@@ -97,7 +44,6 @@ def delete_from_basket(request, item_id):
     **Model**
     :model:`products.Product`
     """
-
     try:
         basket = request.session.get("basket", {})
         product = get_object_or_404(Product, pk=basket[item_id]["product_id"])
@@ -149,19 +95,11 @@ def edit_basket(request, item_id):
     total = calculate_total(request, product)
 
     if request.method == "POST":
-        data = request.POST
-        basket[item_id]["size"] = data.get("size")
-        basket[item_id]["tiers"] = int(data.get("tiers", 1))
-        basket[item_id]["sponge"] = data.get("sponge")
-        basket[item_id]["filling"] = data.get("filling")
-        basket[item_id]["icing"] = data.get("icing")
-        basket[item_id]["main_colour"] = data.get("main_colour")
-        basket[item_id]["secondary_colour"] = data.get("secondary_colour")
-        basket[item_id]["quantity"] = int(data.get("quantity", 1))
-        basket[item_id]["cake_topper"] = data.get("cake_topper")
-        basket[item_id]["total"] = total
 
-        request.session["basket"] = basket
+        # Remove current item from basket to compare it with the updated
+        # choices from user and existing basket to check for qty.
+        del basket[item_id]
+        check_basket(request, basket, product, total)
 
         messages.add_message(
             request, messages.SUCCESS,
